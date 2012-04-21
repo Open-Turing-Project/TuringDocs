52353c0c6b9e5dc8659d32e2f064f5f16d7666cd
Tristan Hume <tris.hume@gmail.com>
2012-04-21 16:31:11 -0400

    Added processed JSON of the documentation.

---
+++ json/limits_maxnat.json
@@ -0,0 +1,8 @@
+{
+  "title": "404: Could not find keyword",
+  "fileName": "limits_maxnat",
+  "sections": [
+
+  ],
+  "htmlcontent": "\n<h1>Keyword not found!</h1>\n<p>\nThe turing keyword could not be found in the online turing doc.<br><a href=\"index-2.html\">You can view the online turing doc here.</a>\n</p>\n"
+}
--- scripts/HTMLProcessor.rb
+++ scripts/HTMLProcessor.rb
@@ -1,9 +1,12 @@
 # Requires nokogiri to function http://http://nokogiri.org/
 require "nokogiri"
 require "pp"
+require "json"
 
 def markdownFormat(htmlStr)
-	text = htmlStr.gsub(/<b>(.*?)<\/b>/,'**\1**')
+	text = htmlStr
+	text = text.gsub(/<a href="(.*?)">(.*?)<\/a>/,'[\1](\2)')
+	text = text.gsub(/<b>(.*?)<\/b>/,'**\1**')
 	text = text.gsub(/<i>(.*?)<\/i>/,'*\1*')
 	# remove html with nokogiri
 	doc = Nokogiri::HTML(text)
@@ -19,13 +22,13 @@ def convFile(f)
 	doc = Nokogiri::HTML(File.open(f))
 	page = {}
 	page[:title] = doc.xpath("/html/head/title/text()").to_s
+	page[:fileName] = f.split("/").last.split(".")[0]
 	page[:sections] = []
-	doc.xpath("//table[2]/tr[@valign=\"top\"]").each do |e|
+	doc.xpath("/html/body/table[2]/tr[@valign=\"top\"]").each do |e|
 		s = {}
 		s[:title] = e.xpath("td[1]/b/text()").to_s.gsub("&nbsp;","").chomp
 
 		#find the examples
-		s[:executables] = []
 		e.xpath("td[2]/object").each do |o|
 			exec = {}
 			button = o.xpath("param[@name=\"Button\"]").first["value"].to_s
@@ -35,16 +38,32 @@ def convFile(f)
 			exec[:name] = commarr[2]
 			exec[:file] = commarr[1]
 
+			s[:executables] ||= []
 			s[:executables] << exec
 			o.remove # remove it from the content, we have processed it
 		end
 
 		#find the code
-		s[:code] = []
 		e.xpath("td[2]//pre").each do |o|
 			code = o.content.to_s
+			s[:code] ||= []
 			s[:code] << code
 			o.remove # remove it from the content, we have processed it
+		end
+
+		#find images
+		e.xpath("td[2]//img").each do |o|
+			file = o["src"]
+			s[:images] ||= []
+			s[:images] << file
+			o.remove # remove it from the content, we have processed it
+		end
+
+		#find links, mostly for processing utility
+		e.xpath("td[2]//a").each do |o|
+			file = o["href"]
+			s[:links] ||= []
+			s[:links] << file[0..-6] # strip .html
 		end
 
 		#format the content
@@ -57,10 +76,20 @@ def convFile(f)
 		end
 		page[:sections] << s
 	end
+
+	#It might be a tutorial page such as turing_admin
+	if page[:sections].empty?
+		page[:htmlcontent] = doc.xpath("/html/body").to_s[6..-8]
+	end
+
 	page
 end
 
-pp convFile("html/font_draw.html")
-# Dir["html/*.html"].each do |f|
-# 	convFile(f)
-# end
+Dir["html/*.html"].each do |f|
+	puts "Processing #{f}"
+	datastruct = convFile(f)
+	json = JSON.pretty_generate(datastruct)
+	File.open("json/" + datastruct[:fileName] + ".json", "w") do |oFile| 
+		oFile.puts json
+	end
+end
